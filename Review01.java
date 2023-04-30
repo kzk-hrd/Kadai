35d1d29ef23ceda5e8943045ac60cc481c38c84f
kzk-hrd <leo.k20504@gmail.com>
2023-04-30 21:43:36 +0900

    03.Javaプログミング入門 課題提出

---
+++ Review01.java
@@ -0,0 +1,17 @@
+public class Review01 {
+
+    public static void main(String[] args) {
+
+        int price = 1500;
+        int tax = tax(price);
+        int result = price + tax;
+        System.out.println(price + "円の税込価格は" + result + "円(消費税は" + tax + "円）です。" );
+    }
+
+    public static int tax(int price) {
+
+        double taxRate = 0.1;
+        int tax = (int) (price * 0.1);
+        return tax;
+    }
+}
