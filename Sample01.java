dc0583f6b5e3eb6bfaa3258ced57f300fcd544da
kzk-hrd <leo.k20504@gmail.com>
2023-04-30 21:44:40 +0900

    03.Javaプログミング入門 課題提出

---
+++ Sample01.java
@@ -0,0 +1,20 @@
+public class Sample01 {
+
+    public static void main(String[] args) {
+
+        int number;
+        String str;
+
+        number = 1;
+        str = "abc";
+
+        System.out.println(number);
+        System.out.println(str);
+
+        int number1 = 100;
+        String str1 = "xyz";
+
+        System.out.print(number1);
+        System.out.print(str1);
+    }
+}
